6daef8d250d8afb86aa0229d2566f34a207a8132
a.demin <a.demin@med-idea.ru>
2024-03-01 13:34:46 +0300

    * add db context, add conf

--- review.test/Program.cs
+++ review.test/Program.cs
@@ -1,10 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using review.test.Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+IServiceCollection services = builder.Services;
 
-builder.Services.AddControllers();
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
+services.AddDbContext<TestDatabase>(opt => opt.UseNpgsql(connectionString));
+
+services.AddControllers();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 
@@ -13,3 +21,12 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+ApplyMigrations(app.Services);
+
+static void ApplyMigrations(IServiceProvider serviceProvider)
+{
+	using IServiceScope scope = serviceProvider.CreateScope();
+	using TestDatabase db = scope.ServiceProvider.GetService<TestDatabase>();
+	db.Database.Migrate();
+}

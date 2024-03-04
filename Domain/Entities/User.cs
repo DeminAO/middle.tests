ecbe0a69e686e16331c3f151e198bd77c3064768
a.demin <a.demin@med-idea.ru>
2024-03-04 04:45:58 +0300

    * extend db context

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,42 @@
+﻿namespace Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public ICollection<UserRole> UserRoles { get; set; } = [];
+}
+
+public class UserRole
+{
+    public Guid UserId { get; set; }
+    public virtual User User { get; set; }
+
+    public Guid RoleId { get; set; }
+    public virtual Role Role { get; set; }
+}
+
+public class Role
+{
+    public Guid Id { get; set; }
+
+	public ICollection<UserRole> UserRoles { get; set; } = [];
+	public ICollection<RolePermission> RolePermissions { get; set; } = [];
+
+}
+
+public class RolePermission
+{
+    public Guid RoleId { get; set; }
+    public virtual Role Role { get; set; }
+
+    public Guid PermissionId { get; set; }
+    public virtual Permission Permission { get; set; }
+}
+
+public class Permission
+{
+	public Guid Id { get; set; }
+	public string Name { get; set; }
+
+	public ICollection<RolePermission> RolePermissions { get; set; } = [];
+}
